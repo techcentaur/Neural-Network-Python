38c12b5799e8590a2cbee9a08e5af7f3aa81bd46
Ankit Solanki <ankit03june@gmail.com>
2018-01-04 18:43:11 +0530

    stochastic_gradient_descent python file re-edited

--- stochastic_gradient_descent.py
+++ stochastic_gradient_descent.py
@@ -2,10 +2,7 @@ import numpy as np
 import random
 
 def stochastic_gradient_descent(training_data, epochs, mini_batch_size, eta, test_data=None):
-	"""Here,
-	1. Train NN using mini-batch SGD.
-	2. If test_data is provided then, network will be evaluated againt test data after every epoch.
-	"""
+	"""Here, we train NN using mini-batch SGD."""
 
 	if test_data:
 		n_test = len(test_data)
@@ -18,8 +15,3 @@ def stochastic_gradient_descent(training_data, epochs, mini_batch_size, eta, tes
 
 		for mini_batch in mini_batches:
 			update_mini_batch(mini_batch,eta)
-
-		if test_data:
-			print ("Epoch "+i+": "+evaluate(test_data)+"/"+n_test)
-		else:
-			print ("Epoch "+i+ " complete!")
